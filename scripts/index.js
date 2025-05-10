7d5e4194625c01351f83c8de1cf24189e554a693
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-05-10 18:40:14 +0530

    chore: modularized the application

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+  fetch('../components/nav.html')
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById('navbar').innerHTML = html;
+        });
+
+        
