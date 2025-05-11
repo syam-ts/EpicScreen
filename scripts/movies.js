972068eb2becd76e024c9e61f88c60de772e6fef
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-05-11 20:27:07 +0530

    feat: add individual page for movie

--- scripts/movies.js
+++ scripts/movies.js
@@ -1,21 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-const container = document.getElementById('movies');
+  const container = document.getElementById('movies');
 
-moviesData.forEach(movie => {
-  const card = document.createElement("div");
-  card.className = "movie-card";
-  card.innerHTML = `
+  moviesData.forEach(movie => {
+    const card = document.createElement("div");
+    card.className = "movie-card";
+    card.innerHTML = `
     <div class="poster">
-    <img src=${movie.poster} alt="Movie Poster">
+    <img class='image' src=${movie.poster} alt="Movie Poster">
   </div>
   <div class="info">
-    <h3 class="title"> ${movie.title} </h3>
+    <h3  class="title"> ${movie.title} </h3>
     <p class="year">Released:  ${movie.year}  </p>
     <p class="director">Director: <span>${movie.director}</span></p>
   </div>
   `;
-  container.appendChild(card)
+    container.appendChild(card);
 
+    const image = card.querySelector(".image");
+    const title = card.querySelector(".title");
+    const movieTitle = title.innerText;
 
-})
+    //sending to individual page
+      image.addEventListener('click', () => { 
+       const encodedTitle = encodeURIComponent(movieTitle)
+       console.log('ecn', encodedTitle)
+       window.location.href = `Movie.html?title=${encodedTitle}`
+  })
+
+  }); 
+
 })
